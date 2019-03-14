e7bc09a7123b2beadae4b6a7a0f7a374b7567882
Fernando Salazar <fernandsalazar3@gmail.com>
2019-03-13 22:04:55 -0300

    added multer for uploads. styled dashboard, TODO: upload and save images, wished books

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' });
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,8 +12,22 @@ router.get('/', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   const { name } = req.query;
 
+  if (!name) {
+    throw Error('Need to provide a name! :(');
+  }
+
   res.render('dashboard', { name });
 });
+
+// router.post('/add-book', upload., function(req, res, next) {
+//   // console.log(req.body)
+//   console.log('FILE')
+//   // console.log(req.file.destination)
+//   console.log(req.files)
+//   // console.log(req.files)
+
+//   res.render('dashboard', { name: 'fernando' });
+// });
 
 
 module.exports = router;
